1ecd2ebf0645dd2ec582d04bd418c2d74a396d73
RAJ RANJAN SRIVASTAV <srivastavrajranjan75@gmail.com>
2023-11-30 16:31:00 +0530

    make footer and worked on some erros regarding data fetchning

--- task25 e-commerce clone/assets/main.js
+++ task25 e-commerce clone/assets/main.js
@@ -47,55 +47,72 @@ fetch("assets/data.json")
       </div>`;
     });
     eleTrg.innerHTML = str;
+  })
+  .then(() => {
+    let itemClick = document.querySelectorAll(".item-click");
+
+    console.log(itemClick.length);
+
+    // woking on ajax
+
+    const xhr = new XMLHttpRequest();
+    //open
+    xhr.open("GET", "assets/ethnic.json", true);
+
+    // onloaad
+
+    xhr.onload = function () {
+      let data = JSON.parse(xhr.responseText);
+      console.log(data, "outer data");
+      console.log(itemClick, "outer data");
+      itemClick.forEach((element) => {
+        console.log(element);
+        element.addEventListener("click", () => {
+          console.log(element, "ele");
+          let holder = "";
+          let contChange = document.getElementById("all-content");
+          console.log(data, "data");
+          data.map((e) => {
+            console.log("andar");
+            let sellprice = (e.showPrice * e.offer) / 100;
+            holder += `
+            <div class="col-2 mt-5 m-2 ">
+              <div class="row mt-5 flex-column">
+                <div class="col box-cont">
+                  <div class="box" style=" background-image: url(${e.img});"></div>
+                  <p class="rating-cont">${e.rating} <i class="bi bi-star-fill rating"></i> | ${e.number}</p>
+                </div>
+                <div class="col ms-2">
+                  <p class="p-0 m-0 mt-2">${e.brand}</p>
+                  <p class="p-0 m-0" style="font-size: 0.8rem">
+                    ${e.type}
+                  </p>
+                  <p class="price">
+                    <span class="fw-bold"> Rs. ${sellprice}</span> <span style="font-size:12px; text-decoration: line-through;">Rs. ${e.showPrice}</span>
+                    <span style="font-size: 12px; color: red;">(${e.offer}% OFF)</span>
+                  </p>
+                </div>
+                <div class="col">
+                  <button class="mainbtn" style="width: 100%">Add To Cart</button>
+                </div>
+                <div class="col">
+                  <button class="mainbtn" style="width: 100%">
+                    Add to Wishlist
+                  </button>
+                </div>
+              </div>
+            </div>
+          `;
+          });
+
+          contChange.innerHTML = holder;
+        });
+      });
+      console.log(itemClick);
+      console.log(data, "last");
+    };
+
+    xhr.send();
   });
 
-// woking on ajax
-const xhr = new XMLHttpRequest();
-//open
-xhr.open("GET", "assets/allitems.html", true);
 
-// onloaad
-xhr.onload = function () {
-  let itemClick = document.getElementsByClassName("box-container");
-
-  for (let item of itemClick) {
-    // console.log(item);
-    item.addEventListener("click", () => {
-      alert("ghjk");
-      let contChange = document.getElementById("all-content");
-      contChange.innerHTML = `<div class="row mt-5">
-      <div class="col-2 mt-5">
-        <div class="row mt-5 flex-column">
-          <div class="col box-cont">
-            <div class="box"></div>
-            <p class="rating-cont">4.3 <i class="bi bi-star-fill rating"></i> | 24K</p>
-          </div>
-          <div class="col ms-2">
-            <p class="p-0 m-0 mt-2">KALINI</p>
-            <p class="p-0 m-0" style="font-size: 0.8rem">
-              kurta with trouser with dupata
-            </p>
-            <p class="price">
-              <span class="fw-bold"> Rs. 850</span> <span style="font-size:12px; text-decoration: line-through;">Rs. 3699</span>
-              <span style="font-size: 12px; color: red;">(77% OFF)</span>
-            </p>
-          </div>
-          <div class="col">
-            <button class="mainbtn" style="width: 100%">Add To Cart</button>
-          </div>
-          <div class="col">
-            <button class="mainbtn" style="width: 100%">
-              Add to Wishlist
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>`;
-    });
-  }
-  // console.log(itemClick);
-  let data = xhr.responseText;
-  // console.log(data);
-};
-
-xhr.send();
