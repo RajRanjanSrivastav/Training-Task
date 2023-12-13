d9bc7907fdbff5cc1195f762ff790073d5e6691e
RAJ RANJAN SRIVASTAV <srivastavrajranjan75@gmail.com>
2023-12-13 14:34:19 +0530

    task completed and implements the themeSeitcher functionality

--- task26 DGTR/assets/main.js
+++ task26 DGTR/assets/main.js
@@ -1,65 +1,72 @@
-const show = (id) => {
+const show = (id,id2) => {
+  console.log(id2);
+  if(id2.classList.contains("bi-x"))
+  {
+    id2.classList.remove("bi-x");
+    id2.classList.add("bi-list");
+  }
+  else{
+    id2.classList.add("bi-x");
+    id2.classList.remove("bi-list");
+  }
+  
   id.classList.toggle("show");
 };
 
 const switchTheme = (id) => {
   let eleTrg = document.querySelectorAll(".wrapper");
 
+  console.log(id);
   // main section
-  let eleMainTrg = document.querySelectorAll('.main');
-  eleMainTrg[0].classList.toggle('dark');
+  let eleMainTrg = document.querySelectorAll(".main");
+  eleMainTrg[0].classList.toggle("dark");
   // console.log(eleMainTrg);
 
-  // for circle
+  // for circle start
   let circleEle = document.querySelectorAll(".circle");
-  let TextContentEle = document.querySelectorAll(".content-box");
-  let swiperSlider = document.querySelectorAll(".mySwiper2");
+  circleEle[0].classList.toggle("dark");
+  circleEle[0].style.border = "2px solid white";
+  // for circle end
 
-  // last slider
+  // for text content of about section start
+  let TextContentEle = document.querySelectorAll(".content-box");
+  TextContentEle.forEach((e) => {
+    e.classList.toggle("dark");
+    e.style.border = "2px solid white";
+  });
+  // for text content of about section end
+
+  let swiperSlider = document.querySelectorAll(".mySwiper2");
+  swiperSlider[0].classList.toggle("dark");
+
+  // last slider start
   let lastSliderEle = document.querySelectorAll(".last-slider");
+  lastSliderEle[0].classList.toggle("dark");
+  // last slider end
 
   // for twitter class
   let twitterEleLight = document.querySelectorAll(".site-light");
   let twitterEleDark = document.querySelectorAll(".site-dark");
   // console.log(circleContentEle);
 
-
-
-
-
-  if (id.classList.contains("black")) {
-
-    twitterEleLight.forEach(e => {
-      e.style.display='block';
+  if (id.classList.contains("bi-moon-fill")) {
+    twitterEleLight.forEach((e) => {
+      e.style.display = "none";
     });
-    twitterEleDark.forEach(e => {
-      e.style.display='none';
+    twitterEleDark.forEach((e) => {
+      e.style.display = "block";
     });
-    id.classList.remove("black");
-    id.classList.add("white");
+    id.classList.remove("bi-moon-fill");
+    id.classList.add("bi-brightness-high-fill");
   } else {
-    twitterEleLight.forEach(e => {
-      e.style.display='none';
+    twitterEleLight.forEach((e) => {
+      e.style.display = "block";
     });
-    twitterEleDark.forEach(e => {
-      e.style.display='block';
+    twitterEleDark.forEach((e) => {
+      e.style.display = "none";
     });
-    id.classList.remove("white");
-    id.classList.add("black");
+    id.classList.remove("bi-brightness-high-fill");
+    id.classList.add("bi-moon-fill");
   }
   eleTrg[0].classList.toggle("dark");
-  circleEle[0].classList.toggle("dark");
-  circleEle[0].style.border = "2px solid white";
-  TextContentEle.forEach((e) => {
-    e.classList.toggle("dark");
-    e.style.border = "2px solid white";
-  });
-
-  
-  swiperSlider[0].classList.toggle("dark");
-  // swiperSlider[0].style.border = "2px solid white";
-
-  // last slider
-  lastSliderEle[0].classList.toggle("dark");
-  // lastSliderEle[0].style.border = "2px solid white";
 };
