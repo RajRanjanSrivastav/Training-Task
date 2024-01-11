00447145bf98de6a3a203c11643e0991437cecbb
RAJ RANJAN SRIVASTAV <srivastavrajranjan75@gmail.com>
2024-01-11 18:40:18 +0530

    crud application is now complete using reactive-form now adding some more functionality

--- task32 formdata into table/go/src/app/components/form-table/form-table.component.ts
+++ task32 formdata into table/go/src/app/components/form-table/form-table.component.ts
@@ -4,7 +4,6 @@ import {
   FormControl,
   FormGroup,
   ReactiveFormsModule,
-  Validator,
   Validators,
 } from '@angular/forms';
 import { StatesService } from '../../../services/states.service';
@@ -16,9 +15,9 @@ import { StatesService } from '../../../services/states.service';
   styleUrl: './form-table.component.css',
 })
 export class FormTableComponent implements OnInit {
-  [x: string]: any;
   stateData: any;
   flag = false;
+  trackBtn = false;
   title = 'REGISTRATION FORM';
   name = '';
   email = '';
@@ -30,17 +29,29 @@ export class FormTableComponent implements OnInit {
   street = '';
   zip = '';
   address = '';
-
   cityArr = [];
+  idNum: number | undefined;
+  temp: any = [];
+  editList: any;
+  indexUpdate: any;
+  trakError = false;
+
   // userForm: any;
-  position = ['Option 1', 'Option 2', 'Option 3'];
-  constructor(private stateServ: StatesService) {}
+  constructor(private stateServ: StatesService) {
+    console.log('kjg');
+  }
+
+  // here we store the state data
   ngOnInit(): void {
+    this.idNum = Math.floor(Math.random() * 100 + 1);
     setTimeout(() => {
       this.stateData = this.stateServ.data;
     }, 1000);
   }
 
+  play() {
+    this.trakError = true;
+  }
   // email = new FormControl('',[Validators.required,Validators.email]);
 
   userForm: FormGroup = new FormGroup({
@@ -59,42 +70,65 @@ export class FormTableComponent implements OnInit {
       Validators.required,
       Validators.pattern('[0-9]{6}'),
     ]),
+    idNum: new FormControl(''),
     address: new FormControl('', [Validators.required]),
   });
 
-  onRegister() {
-    console.log('hjk');
-    // this.name = '';
-    // this.email = '';
-    // this.phone = '';
-    // this.gender = '';
-    // this.pos = '';
-    // this.country = '';
-    // this.city = '';
-    // this.address = '';
-    console.log(this.userForm.valid);
-
-    console.log(this.userForm.value);
+  // fucntion for empty the input field
+  emptyField() {
+    this.name = '';
+    this.email = '';
+    this.phone = '';
+    this.gender = '';
+    this.pos = '';
+    this.state = '';
+    this.city = '';
+    this.street = '';
+    this.zip = '';
+    this.address = '';
   }
 
+  // fucntion for update the input field
+  updateBox(tempList: any) {
+    // console.log(tempList);
+    this.idNum = tempList.idNum;
+    this.name = tempList.name;
+    this.email = tempList.email;
+    this.phone = tempList.phone;
+    this.pos = tempList.position;
+    this.address = tempList.address;
+    this.state = tempList.state;
+    this.city = tempList.city;
+    this.street = tempList.street;
+    this.zip = tempList.zip;
+    this.gender = tempList.gender;
+  }
+
+  // fucntion for add the data in table
+  onRegister() {
+    // console.log('hjk');
+    if (this.userForm.valid) {
+      this.emptyField();
+      let tempId = Math.floor(Math.random() * 100 + 1);
+      if (this.idNum != tempId) {
+        this.idNum = tempId;
+      }
+
+      let sucEle = document.getElementById('suc-box');
+      sucEle!.classList.toggle('show');
+      setTimeout(() => {
+        sucEle!.classList.toggle('show');
+      }, 2000);
+      this.temp.push(this.userForm.value);
+    }
+
+    // console.log(this.temp);
+    // console.log(this.userForm.valid);
+    // console.log(this.userForm.value);
+  }
+
+  // this fucntion for show/hide
   toggel(id1: any, id2: any) {
-    console.log(this.state);
-
-    // console.log(this.stateData[0]);
-
-    // this.stateData.map((e: any) => {
-    //   e.city.map((i: any) => {
-    //     console.log(i.name);
-    //   });
-    //   // console.log(e.city);
-    // });
-
-    // for(let i of this.stateData)
-    // {
-    //   console.log(i.state);
-
-    // }
-
     let form = document.getElementById(id1);
     let table = document.getElementById(id2);
 
@@ -111,14 +145,48 @@ export class FormTableComponent implements OnInit {
 
   // end
 
-  getCity(e:any) {
-    console.log(e);
-    this.cityArr = this.stateData.find(((state: { state: any; })=>state.state===e)).city
-    console.log(this.cityArr);
-    this.cityArr.forEach((i)=>{
-      console.log(i);
-      
-    })
+  // this fucntion for find the cities crorresponding to state
+  getCity(e: any) {
+    this.cityArr = this.stateData.find(
+      (state: { state: any }) => state.state === e
+    ).city;
+  }
+
+  edit(id: any) {
+   
+    this.temp.forEach((e: any, index: any) => {
+  ;
+      if (e.idNum == id) {
     
+        this.indexUpdate = index;
+        this.editList = e;
+      }
+    });
+    // console.log(this.editList);
+
+    this.updateBox(this.editList);
+    this.trackBtn = !this.trackBtn;
+  }
+
+  // for update the element
+  update() {
+    if (this.userForm.valid) {
+      this.emptyField();
+      this.trackBtn = !this.trackBtn;
+      let sucEle = document.getElementById('suc-box');
+      sucEle!.classList.toggle('show');
+      setTimeout(() => {
+        sucEle!.classList.toggle('show');
+      }, 2000);
+      // console.log(this.userForm.value);
+      this.temp[this.indexUpdate] = this.userForm.value;
+    }
+  }
+
+  // for remove the element
+  remove(e: any) {
+    // console.log(e.item);
+    // console.log(this.temp);
+    this.temp = this.temp.filter((j: any) => j != e.item);
   }
 }
